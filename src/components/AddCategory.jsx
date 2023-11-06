211782510f18f4b67e127c52dbf0c1c016af61a5
Kat <moimakatlego@outlook.com>
2023-11-06 14:32:32 +0200

    displaying a list of categories

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,44 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const AddCategory = () => {
+  const [category, setCategory] = useState('');
+  const navigate = useNavigate();
 
-    const [category, setCategory] = useState();
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axios.post('http://localhost:3200/auth/addCategory', {category})
-        .then(result=> console.log(result.data))
-        .catch(err => console.log(err))
-
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:3200/auth/addCategory', { category })
+      .then((response) => {
+        const result = response.data;
+        if (result.Status) {
+          navigate('/dashboard/category');
+        } else {
+          alert(result.error || 'An error occurred');
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        alert('An error occurred');
+      });
+  };
 
   return (
-    <div className='d-flex justify-content-center align-items-center h-75'>
-    <div className='p-3 rounded w-25 border'>
-      <h2>Add Category</h2>
-  
-      <form onSubmit={handleSubmit}>
-        <div className='mb-3'>
-          <label htmlFor='category'>
-            <strong>Category:</strong>
-          </label>
-          <input
-            type='text'
-            name='category'
-            placeholder='Enter Category'
-            className='form-control'
-            onChange={(e) => setCategory(e.target.value)}
-          />
-        </div>
-  
-        <button className='btn btn-success w-100 rounded-0 mb-2'>
-          Add Category
-        </button>
-      </form>
-    </div>
-  </div>
-  )
-}
+    <div className="d-flex justify-content-center align-items-center h-75">
+      <div className="p-3 rounded w-25 border">
+        <h2>Add Category</h2>
 
-export default AddCategory
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="category">
+              <strong>Category:</strong>
+            </label>
+            <input
+              type="text"
+              name="category"
+              placeholder="Enter Category"
+              className="form-control"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            />
+          </div>
+
+          <button className="btn btn-success w-100 rounded-0 mb-2">
+            Add Category
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddCategory;
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Category = () => {
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3200/auth/category")
+      .then((result) => {
+        if (result.data.Status) {
+          setCategory(result.data.Result);
+        } else {
+          alert(result.data.Error);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className='px-5 mt-3'>
       <div className='d-flex justify-content-center'>
@@ -10,6 +26,22 @@ const Category = () => {
       <Link to={"/dashboard/addCategory"} className='btn btn-success'>
         Add Category
       </Link>
+      <div className='mt-3'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {category.map((cat,index) => (
+              <tr  key={index}>
+                <td>{cat.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
