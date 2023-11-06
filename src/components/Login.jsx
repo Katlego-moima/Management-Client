74e91ef82f56ee984febea5019efc433e261722e
Kat <moimakatlego@outlook.com>
2023-11-06 10:33:05 +0200

    adding background image

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React from "react";
+import '../styles/Login.css'
 
 const Login = () => {
-    return (
-        <div className="container text-center">
-            <div className="row  w-100vh">
-                <div className="col-4 bg-primary">
-                    <form>
-                        <label htmlFor="email"><strong>Email:</strong></label>
-                        <input type="email" name='email' autoComplete='off' placeholder='Enter Email' className='form-control' />
-                    </form>
-                </div>
-
-
-            </div>
-        </div>
-    )
-}
-
-export default Login
+  return (
+    <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
+      <div className='p-3 rounded w-25 borde loginForm'>
+        <h2>Login Form</h2>
+        <form>
+          <div className='mb-3'>
+            <label htmlFor='email'>
+              <strong>Email:</strong>
+            </label>
+            <input
+              type='email'
+              name='email'
+              autoComplete='off'
+              placeholder='Enter Email'
+              className='form-control'
+            />
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='password'>
+              <strong>Password:</strong>
+            </label>
+            <input
+              type='password'
+              name='password'
+              autoComplete='off'
+              placeholder='Enter Password'
+              className='form-control'
+            />
+          </div>
+       <button className="btn btn-success w-100 rounded-0 mb-2">Login</button>
+      <div className="mb-1">
+        <input type="checkbox" name="tick" id="tick" className="me-2"/>
+        <label htmlFor="checkbox"> You agree with the terms & conditions</label>
+      </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Login;
